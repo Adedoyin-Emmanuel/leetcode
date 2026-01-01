d61d020a762f9755d673e7e63a808ba0e09557b5
Adedoyin Emmanuel Adeniyi <88517758+Adedoyin-Emmanuel@users.noreply.github.com>
2026-01-01 12:38:14 +0100

    Create 3471-MinimumAverageOfSmallestAndLargestElements

---
+++ 3471-MinimumAverageOfSmallestAndLargestElements/3471-MinimumAverageOfSmallestAndLargestElements.ts
@@ -0,0 +1,21 @@
+// Last updated: 1/1/2026, 12:38:13 PM
+function minimumAverage(nums: number[]): number {
+  var averages: number[] = [];
+  var sortedNums = nums.sort((a, b) => a - b);
+
+  while (sortedNums.length != 0) {
+    var smallest = sortedNums[0];
+    var largest = sortedNums[sortedNums.length - 1];
+
+    var average = (largest + smallest) / 2;
+
+    averages.push(average);
+
+    sortedNums.splice(sortedNums.indexOf(smallest), 1);
+    sortedNums.splice(sortedNums.indexOf(largest), 1);
+  }
+
+  var smallestAverage = Math.min(...averages);
+
+  return smallestAverage;
+}
