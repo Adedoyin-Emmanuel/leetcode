367e57102f8f4b4697827aa04836114f75f9c778
Adedoyin Emmanuel Adeniyi <88517758+Adedoyin-Emmanuel@users.noreply.github.com>
2026-02-01 20:42:23 +0100

    Create 1988-MinimizeMaximumPairSumInArray

---
+++ 1988-MinimizeMaximumPairSumInArray/1988-MinimizeMaximumPairSumInArray.ts
@@ -0,0 +1,16 @@
+// Last updated: 2/1/2026, 8:42:23 PM
+function minPairSum(nums: number[]): number {
+    const sortedNums = nums.sort((num1, num2) => num1 - num2);
+
+    let maxSum = 0;
+    let leftPointer = 0, rightPointer = sortedNums.length - 1;
+
+    while(leftPointer < rightPointer){
+        maxSum = Math.max(maxSum, nums[leftPointer] + nums[rightPointer]);;
+
+        leftPointer += 1;
+        rightPointer -= 1;
+    }
+
+    return maxSum;
+};
