0142d8e240b2eaa8c8a9f3f32c87c1e2e662a2ac
Adedoyin Emmanuel Adeniyi <88517758+Adedoyin-Emmanuel@users.noreply.github.com>
2026-02-01 20:42:12 +0100

    Create 3611-ConstructTheMinimumBitwiseArrayIi

---
+++ 3611-ConstructTheMinimumBitwiseArrayIi/3611-ConstructTheMinimumBitwiseArrayIi.ts
@@ -0,0 +1,42 @@
+// Last updated: 2/1/2026, 8:42:11 PM
+function minBitwiseArray(nums: number[]): number[] {
+    /**
+        Just as expected lmao. I knew today's question will be part 2 of
+        yesterday's question.
+
+        Low-key waking up to solve Leetcode problems everyday gives me dopamine boost
+
+        Now let's get to it, no time.
+
+        The goal of this problem is to construct the minimum bitwise 
+        array.
+
+        This is very similar to yesterdays' solution, I just have to 
+        unset the previous bit
+     */
+
+     let result = [];
+
+     for(let i = 0; i < nums.length; i++){
+        const currentNumber = nums[i];
+
+        if(currentNumber % 2 === 0){
+            result.push(-1);
+            continue;
+        }
+
+
+        let candidate = currentNumber;
+        let bit = 0;
+
+        while(((candidate >> bit) & 1) == 1){
+            bit += 1;
+        }
+
+        candidate = candidate ^ (1 << (bit - 1));
+
+        result.push(candidate);
+     }
+
+     return result;
+};
