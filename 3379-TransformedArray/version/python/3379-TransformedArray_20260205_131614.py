e0b006d0389f6641bd635ceef724b9a0c4143090
Adedoyin Emmanuel Adeniyi <88517758+Adedoyin-Emmanuel@users.noreply.github.com>
2026-02-05 13:16:15 +0100

    Create 3379-TransformedArray

---
+++ 3379-TransformedArray/version/python/3379-TransformedArray_20260205_131614.py
@@ -0,0 +1,12 @@
+# Last updated: 2/5/2026, 1:16:14 PM
+1class Solution:
+2    def constructTransformedArray(self, nums: List[int]) -> List[int]:
+3        n = len(nums)
+4        result = nums.copy()
+5
+6        for i in range(n):
+7            newIndex = ((i + nums[i]) % n + n) % n
+8            result[i] = nums[newIndex]
+9
+10        return result
+11        
