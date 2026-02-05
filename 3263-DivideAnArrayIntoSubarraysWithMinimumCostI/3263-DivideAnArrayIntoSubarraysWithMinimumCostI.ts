6f5872838efeb4a075bfc171861e9768b300d98c
Adedoyin Emmanuel Adeniyi <88517758+Adedoyin-Emmanuel@users.noreply.github.com>
2026-02-05 07:08:32 +0100

    Create 3263-DivideAnArrayIntoSubarraysWithMinimumCostI

---
+++ 3263-DivideAnArrayIntoSubarraysWithMinimumCostI/3263-DivideAnArrayIntoSubarraysWithMinimumCostI.ts
@@ -0,0 +1,18 @@
+// Last updated: 2/5/2026, 7:08:31 AM
+function minimumCost(nums: number[]): number {
+    let firstCost = nums[0];
+    let minimumSoFar = nums[1];
+    let minimumCost = Infinity;
+
+    for(let i = 2; i < nums.length; i++){
+        const candidateCost = firstCost + minimumSoFar + nums[i];
+
+        if(minimumCost > candidateCost){
+            minimumCost = candidateCost;
+        }
+
+        minimumSoFar = Math.min(minimumSoFar, nums[i]);
+    }
+
+    return minimumCost;
+};
