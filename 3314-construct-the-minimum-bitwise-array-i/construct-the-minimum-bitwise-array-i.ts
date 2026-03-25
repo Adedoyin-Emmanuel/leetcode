8602b799833f23f87107e45a9a146fd70aa366e9
Adedoyin Emmanuel Adeniyi <88517758+Adedoyin-Emmanuel@users.noreply.github.com>
2026-03-25 04:44:57 +0100

    Solve Construct the Minimum Bitwise Array I

---
+++ 3314-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.ts
@@ -0,0 +1,34 @@
+function minBitwiseArray(nums: number[]): number[] {
+    /**
+        Ah easy keh? Ki la ri gbo?
+
+        This should be relatively simple, let's see.
+
+        I'm a bit confused about the bitwise operators, I've to look into it.
+     */
+
+     let result = [];
+
+     for(let i = 0; i < nums.length; i++){
+        const currentNumber = nums[i];
+
+        if(currentNumber == 2){
+            result.push(-1);
+            continue
+        }
+
+        let temp = currentNumber;
+        let trailingOnes = 0;
+
+        while((temp & 1) == 1){
+            trailingOnes += 1;
+            temp = temp >> 1;
+        }
+
+        let candidate = currentNumber - (1 << (trailingOnes - 1));
+
+        result.push(candidate);
+     }
+
+     return result;
+};
