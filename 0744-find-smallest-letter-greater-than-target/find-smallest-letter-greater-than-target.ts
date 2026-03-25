857e162914053dfb51be8dfb3430cfdfe339bf50
Adedoyin Emmanuel Adeniyi <88517758+Adedoyin-Emmanuel@users.noreply.github.com>
2026-03-25 04:52:41 +0100

    Solve Find Smallest Letter Greater Than Target

---
+++ 0744-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.ts
@@ -0,0 +1,23 @@
+function nextGreatestLetter(letters: string[], target: string): string {  
+
+    //brute-force method, I will use binary search later 
+
+    let result = letters[0];
+
+    
+
+    
+
+    
+    for(let i = 0; i < letters.length; i++){
+        if(letters[i] > target){
+            result = letters[i]
+            break;
+        }
+    }
+
+
+
+    return result;
+
+};
