8434b57fcb883b27ac80b1834c21071e04297e3a
Adedoyin Emmanuel Adeniyi <88517758+Adedoyin-Emmanuel@users.noreply.github.com>
2026-05-31 21:10:30 +0100

    Solve Destroying Asteroids

---
+++ medium/2126-destroying-asteroids/destroying-asteroids.ts
@@ -0,0 +1,15 @@
+function asteroidsDestroyed(mass: number, asteroids: number[]): boolean {
+    
+    asteroids.sort((a, b) => a - b); 
+
+    let currentMass = mass;
+
+    for (const asteroid of asteroids) {
+        
+        if (currentMass < asteroid) {
+            return false;
+        }
+        currentMass += asteroid;
+    }
+    return true; 
+}
