9dfb8ab5609e9a5265f7aab796cf1445affea406
Adedoyin Emmanuel Adeniyi <88517758+Adedoyin-Emmanuel@users.noreply.github.com>
2026-08-01 06:15:25 +0100

    Solve Minimum Number of Pushes to Type Word II

---
+++ medium/3016-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.ts
@@ -0,0 +1,20 @@
+function minimumPushes(word: string): number {
+    const counts: number[] = new Array(26).fill(0);
+
+    for (const char of word) {
+        counts[char.charCodeAt(0) - 97]++;
+    }
+
+    counts.sort((a, b) => b - a);
+    let total = 0;
+
+    for (let i = 0; i < 26; i++) {
+        if (counts[i] === 0) {
+            break;
+        }
+
+        total += counts[i] * (Math.floor(i / 8) + 1);
+    }
+
+    return total;
+}
