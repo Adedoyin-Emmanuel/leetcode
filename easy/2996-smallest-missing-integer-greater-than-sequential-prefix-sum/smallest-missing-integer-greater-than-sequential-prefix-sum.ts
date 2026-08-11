88859a46303107e22f5134b2e9fa41f2fec7e0b1
Adedoyin Emmanuel Adeniyi <88517758+Adedoyin-Emmanuel@users.noreply.github.com>
2026-08-11 01:13:41 +0100

    Solve Smallest Missing Integer Greater Than Sequential Prefix Sum

---
+++ easy/2996-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.ts
@@ -0,0 +1,19 @@
+function missingInteger(nums: number[]): number {
+    const n = nums.length;
+    const numSet = new Set(nums);
+    let total = nums[0];
+
+    for (let i = 1; i < n; i++) {
+        if (nums[i] === nums[i - 1] + 1) {
+            total += nums[i];
+        } else {
+            break;
+        }
+    }
+
+    while (numSet.has(total)) {
+        total += 1;
+    }
+
+    return total;
+}
